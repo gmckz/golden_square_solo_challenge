87703cf9f3e163d2c0f55747c86179f6e62acabe
gmckz <gemma@mastudedvqgrq54.lan>
2023-06-29 15:29:23 +0100

    Confirmation_text.py updated with environment variables, test written
    git status

--- lib/confirmation_text.py
+++ lib/confirmation_text.py
@@ -13,8 +13,8 @@ class ConfirmationText():
 
         # Find your Account SID and Auth Token at twilio.com/console
         # and set the environment variables. See http://twil.io/secure
-        account_sid = os.environ['AC2081cba849ca4efd1d6fb08b5b57c696']
-        auth_token = os.environ['cb7deee627b37124239ab02e6954c42a']
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
         client = Client(account_sid, auth_token)
 
         message = client.messages \
--- tests/test_confirmation_text.py
+++ tests/test_confirmation_text.py
@@ -1,6 +1,7 @@
 from lib.confirmation_text import *
 from datetime import datetime
+from datetime import timedelta
 
-# def test_send_sms_prints_message():
-#     text = ConfirmationText()
-#     assert text.send_sms(datetime.now()) == 
+def test_send_sms_prints_message():
+    text = ConfirmationText()
+    assert text.send_sms(datetime.now()) == f"Thank you! Your order was placed and will be delivered before {datetime.now() + timedelta(minutes=30)}"
